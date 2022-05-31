2ce5a9552d20f1d03e33c25a8125516751b3c860
ken_d <ken_douglas@btinternet.com>
2022-05-31 11:27:02 +0100

    Added comments

--- src/co/uk/mycomputerworld/Application.java
+++ src/co/uk/mycomputerworld/Application.java
@@ -11,8 +11,13 @@ public class Application {
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
         System.out.println("HELLO WORLD");
+
         String[] beanNames = ctx.getBeanDefinitionNames();
+
+        /* Display all the beans defined in the Spring application context
+         *  They are Auto-configured by the Spring Boot auto-configuration */
         Arrays.sort(beanNames);
         for (String beanName : beanNames) {
             System.out.println(beanName);
